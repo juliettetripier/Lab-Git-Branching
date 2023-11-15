9bbed2b4e1dc29a037876d80479a23636a8db961
Franciane Schipits <franmifreitas@gmail.com>
2023-11-15 14:10:42 -0600

    function-person-b-another-commit

--- function-person-b.py
+++ function-person-b.py
@@ -1,6 +1,7 @@
 #Build a function that takes a number, and returns that number multiplied by 100.
 
 def multiplied_by_100():
+    """another commit"""
     num=input('Enter num:')
     return int(num) * 100
 print(multiplied_by_100())

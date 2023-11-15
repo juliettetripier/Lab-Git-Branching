7b30f8f65480eab7e33b95f4de7b2d3163dd9a44
Franciane Schipits <franmifreitas@gmail.com>
2023-11-15 14:09:43 -0600

    function-person-b

---
+++ function-person-b.py
@@ -0,0 +1,6 @@
+#Build a function that takes a number, and returns that number multiplied by 100.
+
+def multiplied_by_100():
+    num=input('Enter num:')
+    return int(num) * 100
+print(multiplied_by_100())
